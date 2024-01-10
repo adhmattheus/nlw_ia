03d8d26168020805140508e2bf4b15b5ba58f39f
adhmattheus <mattheusadhonnaysf@hotmail.com>
2024-01-10 11:36:10 -0300

    finalizando o front-end da aplicação

--- mastery/web/upload-ai-web/src/App.tsx
+++ mastery/web/upload-ai-web/src/App.tsx
@@ -1,11 +1,15 @@
-import { Button } from "./components/ui/button";
-import { Github } from 'lucide-react'
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./components/ui/select";
+import { FileVideo, Github, Upload, Wand2 } from 'lucide-react'
 import { Separator } from "./components/ui/separator";
 import { Textarea } from "./components/ui/textarea";
+import { Button } from "./components/ui/button";
+import { Slider } from "./components/ui/slider";
+import { Label } from "./components/ui/label";
 
 export function App() {
   return (
     <div className="min-h-screen flex flex-col" >
+
       <div className="px-6 py-3 flex items-center justify-between border-b">
         <h1 className="text-xl font-bold">upload.ai</h1>
 
@@ -22,7 +26,6 @@ export function App() {
           </Button>
 
         </div>
-
       </div>
 
       <main className="flex-1 p-6 flex gap-6">
@@ -48,10 +51,81 @@ export function App() {
 
         </div>
 
-        <aside className="w-80">
-          
-        </aside>
+        <aside className="w-80 space-y-6 ">
 
+          <form className="space-y-6">
+            <label
+              htmlFor="video"
+              className="border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/50">
+
+              <FileVideo className="w-4 h-4" />
+              Carregar video
+            </label>
+
+            <input type="file" id="video" accept="video/mp4" className="sr-only" />
+
+            <Separator />
+
+            <div className="space-y-2">
+              <Label htmlFor="transcription_prompt ">Prompt de transcrição</Label>
+              <Textarea id="transcription_prompt" className="min-h-20 resize-none leading-relaxed" placeholder="Inclua palavras-chaves mencionadas no vídeo separadas por vírgula (,)" />
+            </div>
+
+            <Button type="submit" className="w-full">
+              Carregar vídeo
+              <Upload className="w-4 h-4 ml-2 " />
+            </Button>
+          </form>
+
+          <Separator />
+
+          < form className="space-y-6">
+
+            <div className="space-y-2">
+              <Label>Prompt</Label>
+              <Select  >
+                <SelectTrigger>
+                  <SelectValue placeholder="Selecione um prompt" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="title">Título do Youtube</SelectItem>
+                  <SelectItem value=" description">Descrição do Youtube</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+
+            <div className="space-y-2">
+              <Label>Modelo</Label>
+              <Select disabled defaultValue="gpt 3.5">
+                <SelectTrigger>
+                  <SelectValue />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="gpt 3.5">GPT 3.5-turbo 16k</SelectItem>
+                </SelectContent>
+              </Select>
+              <span className="block text-xs text-muted-foreground">Você pdoerá customizar essa opção em breve.</span>
+            </div>
+
+            <Separator />
+
+            <div className="space-y-2">
+              <Label>Temperatura</Label>
+              <Slider min={0} max={1} step={0.1} />
+
+              <span className="block text-xs text-muted-foreground">
+                Valores mais altos tendem a deixar os resultados mais criativos e com possíveis erros.
+              </span>
+            </div>
+
+            <Separator />
+
+            <Button type="submit" className="w-full">
+              Executar
+              <Wand2 className="w-4 h-4 ml-2" />
+            </Button>
+          </form>
+        </aside>
       </main>
     </div>
   )
