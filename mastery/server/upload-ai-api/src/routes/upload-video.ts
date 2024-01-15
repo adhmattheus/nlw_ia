361b4961a346947eecefdb6537765632870117ab
adhmattheus <mattheusadhonnaysf@hotmail.com>
2024-01-15 16:53:23 -0300

    corrigindo error de criação de video

--- mastery/server/upload-ai-api/src/routes/upload-video.ts
+++ mastery/server/upload-ai-api/src/routes/upload-video.ts
@@ -1,37 +1,32 @@
-import { fastifyMultipart } from '@fastify/multipart'
 import { FastifyInstance } from "fastify";
-import { randomUUID } from "node:crypto";
-import { prisma } from "../lib/prisma";
-import { promisify } from 'node:util';
-import pipeline from "node:stream";
+import { fastifyMultipart } from "@fastify/multipart";
 import path from "node:path";
+import { randomUUID } from "node:crypto";
 import fs from "node:fs";
+import { pipeline } from "node:stream";
+import { promisify } from "node:util";
+import { prisma } from "../lib/prisma";
 
 const pump = promisify(pipeline)
 
 export async function uploadVideoRoute(app: FastifyInstance) {
   app.register(fastifyMultipart, {
     limits: {
-      fileSize: 1_048_576 * 25, // 25MB
-    },
-  });
+      fileSize: 1_048_576 * 25, // 25mb
+    }
+  })
 
-  app.post('/videos', async (request, reply) => {
-    const data = await request.file();
-
+  app.post('/videos', async (request, replay) => {
+    const data = await request.file()
 
     if (!data) {
-      return reply.status(400).send({
-        error: 'Missing file input.',
-      });
+      return replay.status(400).send({ error: 'Missing file input.' })
     }
 
-    const extension = path.extname(data.filename);
+    const extension = path.extname(data.filename)
 
-    if (extension != '.mp3') {
-      return reply.status(400).send({
-        error: 'Invalid input type, please upload a .mp3 file.',
-      });
+    if (extension !== '.mp3') {
+      return replay.status(400).send({ error: 'Invalid input type, please upload a .mp3 file.' })
     }
 
     const fileBaseName = path.basename(data.filename, extension)
@@ -43,10 +38,12 @@ export async function uploadVideoRoute(app: FastifyInstance) {
     const video = await prisma.video.create({
       data: {
         name: data.filename,
-        path: uploadDestination
+        path: uploadDestination,
       }
     })
 
-    return { video, }
+    return {
+      video
+    }
   })
 }
