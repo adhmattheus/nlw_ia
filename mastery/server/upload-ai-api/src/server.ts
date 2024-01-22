de914f30d1f5cc1a22f44451fdeedb284cf4528a
adhmattheus <mattheusadhonnaysf@hotmail.com>
2024-01-22 13:18:50 -0300

    generate transcription route

--- mastery/server/upload-ai-api/src/server.ts
+++ mastery/server/upload-ai-api/src/server.ts
@@ -1,13 +1,20 @@
 import { createTranscriptionRoute } from './routes/create-transcription'
+import { generateAiCompletionRoute } from './routes/generate-ai-completion'
 import { getAllPromptsRoutes } from './routes/get-all-prompts'
 import { uploadVideoRoute } from './routes/upload-video'
 import { fastify } from 'fastify'
+import { fastifyCors } from '@fastify/cors'
 
 const app = fastify()
+
+app.register(fastifyCors, {
+  origin: '*',
+})
 
 app.register(getAllPromptsRoutes)
 app.register(uploadVideoRoute)
 app.register(createTranscriptionRoute)
+app.register(generateAiCompletionRoute)
 
 app.listen({
   port: 3333,
